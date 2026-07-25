3584bd74610bbff76efe9046544b82503aae7322
Martin Brenn <brenn@depon.net>
2026-07-25 12:40:46 +0200

    Updated Project versions for 1.0.1

--- scripts/51_publish_dotnet_tool.ps1
+++ scripts/51_publish_dotnet_tool.ps1
@@ -5,6 +5,6 @@ cd ../src/Tool/BSMake
 
 # nuget setapikey {key} --source https://api.nuget.org/v3/index.json <-- currently not working under linux
 
-dotnet nuget push nupkg/BSMake.1.0.0.nupkg --source https://api.nuget.org/v3/index.json --api-key {key}
+dotnet nuget push nupkg/BSMake.1.0.1.nupkg --source https://api.nuget.org/v3/index.json --api-key {key}
 
 cd ../../../scripts
