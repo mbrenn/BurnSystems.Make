0d2c864745733aabaf537bbfd6931f307ab0ef5b
Martin Brenn <brenn@depon.net>
2026-07-25 12:56:07 +0200

    We moved to bsmake as a direct call

--- src/Example/Example.Library/.bsmake/Build.cs
+++ src/Example/Example.Library/.bsmake/Build.cs
@@ -1,9 +1,6 @@
-namespace Example.Library.bsmake;
 
 File.WriteAllText(
     "Primes.cs",
     "public class Primes\n{\npublic static int[] PrimesTill20 = [2, 3, 5, 7, 11, 13, 17, 19];\n}");
 
 Console.WriteLine("Primes.cs written");
-
-S
--- src/Example/Example.Library/Primes.cs
+++ src/Example/Example.Library/Primes.cs
@@ -1,4 +1,4 @@
 public class Primes
 {
-    public static int[] PrimesTill20 = [2, 3, 5, 7, 11, 13, 17, 19];
+public static int[] PrimesTill20 = [2, 3, 5, 7, 11, 13, 17, 19];
 }
