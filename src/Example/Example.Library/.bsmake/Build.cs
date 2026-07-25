27f12d853be221c1bd8e3eb85dcf13ae434ef11f
Martin Brenn <brenn@depon.net>
2026-07-25 12:54:19 +0200

    Simplified Example-Buildskript

--- src/Example/Example.Library/.bsmake/Build.cs
+++ src/Example/Example.Library/.bsmake/Build.cs
@@ -1,18 +1,9 @@
 namespace Example.Library.bsmake;
 
-public class Build
-{
-    public static void Main()
-    {
-        File.WriteAllText(
-            "Primes.cs",
-"""
-public class Primes
-{
-    public static int[] PrimesTill20 = [2, 3, 5, 7, 11, 13, 17, 19];
-}
-""");
-        
-        Console.WriteLine("Primes.cs written");
-    }
-}
+File.WriteAllText(
+    "Primes.cs",
+    "public class Primes\n{\npublic static int[] PrimesTill20 = [2, 3, 5, 7, 11, 13, 17, 19];\n}");
+
+Console.WriteLine("Primes.cs written");
+
+S
