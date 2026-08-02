0b492078c5c646242c15adf74bc94a6702678dba
Martin Brenn <brenn@depon.net>
2026-08-02 11:41:30 +0200

    Extended Debug message

--- src/Tool/BurnSystems.Make/BuildAgentLogic.cs
+++ src/Tool/BurnSystems.Make/BuildAgentLogic.cs
@@ -209,11 +209,11 @@ public class BuildAgentLogic(CommandLineArguments arguments)
         switch (exeFiles.Length)
         {
             case 0: 
-                throw new InvalidOperationException($"No executable found in bin directory: {BuildAgentDirBinary}");
+                throw new InvalidOperationException($"No executable found in bin directory: {BuildAgentDirBinary} for {buildAgentInformation.OutputAssemblyName}");
             case 1:
                 break;
             default:
-                throw new InvalidOperationException($"Multiple executables found in bin directory: {BuildAgentDirBinary}");
+                throw new InvalidOperationException($"Multiple executables found in bin directory: {BuildAgentDirBinary} for {buildAgentInformation.OutputAssemblyName}");
         }
         
         Logger.Info($"Calling executable: {exeFiles.Single()}");
